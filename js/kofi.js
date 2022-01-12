2f6f9e761b239f79c9366676e04fa366b327b501
ProfessorLaw <ProfessorLaw@users.noreply.github.com>
2022-01-12 21:14:46 +0000

    deploy: 5c4bcc7bb9deec9eef6a318096a2191f6c0cab55

---
+++ js/kofi.js
@@ -0,0 +1,6 @@
+kofiWidgetOverlay.draw('ProfessorLaw', {
+    'type': 'floating-chat',
+    'floating-chat.donateButton.text': 'Support me',
+    'floating-chat.donateButton.background-color': '#006cd1',
+    'floating-chat.donateButton.text-color': '#fff'
+});
